d6b2d320952b072312c0ff6448da83158d0c9c4c
Matthew Baugh <matthew.baugh17@imperial.ac.uk>
2019-10-24 17:40:50 +0100

    Make visitor for simulator command generation

--- xdrone/__init__.py
+++ xdrone/__init__.py
@@ -1,6 +1,13 @@
 from xdrone.parser import xdrone_parser
 from xdrone.visitors.fly import Fly
+from xdrone.visitors.simulate import Simulate
 
 def fly(program, addr = "d0:3a:86:9d:e6:5a"):
     parse_tree = xdrone_parser.parse(program)
     Fly(addr).visit(parse_tree)
+
+def gen_simulate_commands(program):
+    parse_tree = xdrone_parser.parse(program)
+    simulator = Simulate()
+    simulator.visit(parse_tree)
+    return simulator.commands
--- xdrone/visitors/simulate.py
+++ xdrone/visitors/simulate.py
@@ -1,18 +1,25 @@
 from logging import info
 from lark import Visitor
+from math import radians
 
 class Simulate(Visitor):
+
+
     def __init__(self):
         self.commands = []
 
+  
     def add_command(self, command, val):
-        self.commands.append({action: command, value: val})
+        self.commands.append({"action": command, "value": val})
+
 
     def takeoff(self, tree):
         self.add_command("up", 2.5)
 
+
     def land(self, tree):
         self.add_command("down", 2.5)
+
 
     def up(self, tree):
         duration, = tree.children
@@ -20,11 +27,13 @@ class Simulate(Visitor):
 
         self.add_command("up", duration)
 
+
     def down(self, tree):
         duration, = tree.children
         duration = int(duration)
 
         self.add_command("down", duration)
+
 
     def left(self, tree):
         duration, = tree.children
@@ -32,11 +41,13 @@ class Simulate(Visitor):
 
         self.add_command("left", duration)
 
+
     def right(self, tree):
         duration, = tree.children
         duration = int(duration)
 
         self.add_command("right", duration)
+
 
     def forward(self, tree):
         duration, = tree.children
@@ -44,26 +55,33 @@ class Simulate(Visitor):
 
         self.add_command("forward", duration)
 
+
     def backward(self, tree):
         duration, = tree.children
         duration = int(duration)
 
         self.add_command("backwards", duration)
 
-    def rotatel(self, tree):
-        duration, = tree.children
-        duration = int(duration)
 
-        self.add_command("up", duration)
+    def rotatel(self, tree):
+        degrees, = tree.children
+        degrees = int(degrees)
+
+        self.add_command("rotateL", radians(degrees))
+
 
     def rotater(self, tree):
-        duration, = tree.children
-        duration = int(duration)
+        degrees, = tree.children
+        degrees = int(degrees)
 
-        self.add_command("up", duration)
+        self.add_command("rotateR", radians(degrees))
+
 
     def wait(self, tree):
         duration, = tree.children
         duration = int(duration)
 
-        self.add_command("up", duration)
+        self.add_command("wait", duration)
+
+
+
