9e203f151708e0aea9fef3978f17e64721e1a18a
Matthew Baugh <matthew.baugh17@imperial.ac.uk>
2019-10-24 17:53:25 +0100

    Add simulate endpoint to flask server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonsify
 from flask_cors import CORS
 import json
 import xdrone
@@ -6,13 +6,16 @@ import xdrone
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/test')
 def test():
     return "Hello world from flask backend!"
 
+
 @app.route('/')
 def root():
     return "Hello xDrone!"
+
 
 @app.route('/fly', methods=['POST'])
 def fly():
@@ -20,6 +23,15 @@ def fly():
     program = data['body']['body']['program']
     xdrone.fly(program)
     return "Flight success"
+
+
+@app.route('/simulate', methods=['POST'])
+def simulate():
+    data = request.get_json()
+    program = data['body']['body']['program']
+    commands = xdrone.gen_simulate_commands(program)
+    return jsonify({'commands': commands})
+
 
 if __name__ =="__main__":
     app.run(host='0.0.0.0',debug=True,port=8081)
