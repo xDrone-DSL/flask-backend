cbadcac521b7385d02fdf171e58fbd950ba2ce98
George Ordish <george.ordish17@imperial.ac.uk>
2019-11-08 15:44:05 +0000

    Clean up validator

--- xdrone/visitors/validate.py
+++ xdrone/visitors/validate.py
@@ -2,6 +2,7 @@ from logging import info
 from lark import Visitor
 from math import cos, sin, pi, radians
 
+
 class Validate(Visitor):
 
     def __init__(self):
@@ -10,14 +11,6 @@ class Validate(Visitor):
         self.min_x, self.min_y, self.min_z = 0, 0, 0
         self.max_x, self.max_y, self.max_z = 0, 0, 0
 
-    def __del__(self):
-        print("min x: {}, y: {}, z: {}", self.min_x, self.min_y, self.min_z)
-        print("max x: {}, y: {}, z: {}", self.max_x, self.max_y, self.max_z)
-
-    def print_state(self):
-        pass
-        #print("x: {}, y: {}, z: {}, dir: {}".format(self.x, self.y, self.z,
-                                                    #self.direction))
     def travel(self, angle, duration):
         self.x -= duration * sin(angle)
         self.y += duration * cos(angle)
@@ -29,62 +22,48 @@ class Validate(Visitor):
         self.max_z = max(self.max_z, self.z)
 
     def up(self, tree):
-        self.print_state()
         duration, = tree.children
         duration = float(duration)
 
         self.z += duration
 
     def down(self, tree):
-        self.print_state()
         duration, = tree.children
         duration = float(duration)
 
         self.z -= duration
 
-
     def left(self, tree):
-        self.print_state()
         duration, = tree.children
         duration = float(duration)
 
         self.travel(self.direction + pi/2, duration)
 
-
     def right(self, tree):
-        self.print_state()
         duration, = tree.children
         duration = float(duration)
 
         self.travel(self.direction - pi/2, duration)
 
-
     def forward(self, tree):
-        self.print_state()
         duration, = tree.children
         duration = float(duration)
 
         self.travel(self.direction, duration)
 
-
     def backward(self, tree):
-        self.print_state()
         duration, = tree.children
         duration = float(duration)
 
         self.travel(self.direction - pi, duration)
 
-
     def rotatel(self, tree):
-        self.print_state()
         degrees, = tree.children
         rads = radians(float(degrees))
 
         self.direction += rads
 
-
     def rotater(self, tree):
-        self.print_state()
         degrees, = tree.children
         rads = radians(float(degrees))
 
